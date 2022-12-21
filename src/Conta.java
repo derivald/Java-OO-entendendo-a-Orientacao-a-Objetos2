2e8dc86baff5217ea282b68e0e2235a8b699cebe
Derivaldo Brito <derivaldo.brito@yahoo.com.br>
2022-12-20 22:48:53 -0300

    Aplicando excecoes

--- src/Conta.java
+++ src/Conta.java
@@ -1,17 +1,16 @@
-public abstract class Conta{
+public abstract class Conta {
 	protected double saldo;
 	private int agencia;
 	private int numero;
 	private Cliente titular;
 	private static int total = 0;
-	
-	
+
 	public Conta() {
-		
+
 	}
-	
+
 	public Conta(int agencia, int numero) {
-		
+
 		Conta.total++;
 //		System.out.println("O total de contas é " + Conta.total);
 		this.agencia = agencia;
@@ -19,60 +18,66 @@ public abstract class Conta{
 //		this.saldo = 100;
 //		System.out.println("Estou criando uma conta" + this.numero);
 	}
-	
+
 	public abstract void deposita(double valor);
-	public boolean saca(double valor){
-		if (this.saldo >= valor) {
-			this.saldo -=  valor;
-			return true;
-		}else {
-			return false;
+
+	public void saca(double valor) throws SaldoInsuficienteException {
+
+		if (this.saldo < valor) {
+			//problema
+			throw new SaldoInsuficienteException("Saldo: " + this.saldo + ", Valor: " + valor);
 		}
+		this.saldo -= valor;
+
 	}
-	public boolean transfere(double valor, Conta destino) {
-		if(this.saca(valor)) {
-			
-			destino.deposita(valor);
-			return true;
-		}
-			return false;
-		
+
+	public void transfere(double valor, Conta destino) throws SaldoInsuficienteException {
+		this.saca(valor);
+		destino.deposita(valor);
+	
+
 	}
+
 	public double getSaldo() {
 		return this.saldo;
 	}
+
 	public int getNumero() {
 		return this.numero;
 	}
-	public void setNumero(int numero){
-		if(numero <= 0) {
+
+	public void setNumero(int numero) {
+		if (numero <= 0) {
 			System.out.println("não pode valor menor ou igual a 0");
 			return;
 		}
 		this.numero = numero;
-		
+
 	}
+
 	public int getAgencia() {
 		return this.agencia;
-		}
-		
-	
+	}
+
 	public void setAgencia(int agencia) {
-		if(agencia <= 0) {
+		if (agencia <= 0) {
 			System.out.println("não pode valor menor ou igual a zero");
 			return;
 		}
 		this.agencia = agencia;
 	}
+
 	public Cliente getTitular() {
 		return titular;
 	}
+
 	public void setTitular(Cliente titular) {
 		this.titular = titular;
 	}
+
 	public static int getTotal() {
-		
+
 		return Conta.total;
 	}
-	
+
 }
